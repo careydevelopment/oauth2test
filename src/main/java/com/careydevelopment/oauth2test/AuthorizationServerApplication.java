43758479df9dda93b759461c256e9c7c4b64849d
Brian Carey <mrbrianmcarey@gmail.com>
2018-11-17 16:32:12 -0500

    Added jwt

--- src/main/java/com/careydevelopment/oauth2test/AuthorizationServerApplication.java
+++ src/main/java/com/careydevelopment/oauth2test/AuthorizationServerApplication.java
@@ -5,15 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-import com.careydevelopment.oauth2test.model.User;
-
 @SpringBootApplication
 @EnableResourceServer
 public class AuthorizationServerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthorizationServerApplication.class, args);
-        
     }
 
 }
